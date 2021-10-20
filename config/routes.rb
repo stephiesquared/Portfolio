0ba02bbd7555ea1758be552dae21ea5d48ca34a1
stephiesquared <57155219+stephiesquared@users.noreply.github.com>
2021-10-20 12:55:39 -0600

    reset

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root "projects#index"
-  resources :projects
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
